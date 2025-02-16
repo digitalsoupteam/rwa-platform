d758815e0c1fe98b1c030a6194b51a7c02dd65fd
inveker <alex.korolev.1995@gmail.com>
2025-02-16 15:54:16 +0300

    factory tests

--- contracts/rwa/Factory.sol
+++ contracts/rwa/Factory.sol
@@ -6,13 +6,14 @@ import { ERC1155Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ER
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import { MessageHashUtils } from "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
 import { SignatureChecker } from "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import { ReentrancyGuardUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
 
 import { AddressBook } from "../system/AddressBook.sol";
 import { RWA } from "./RWA.sol";
 import { Pool } from "./Pool.sol";
 import { Config } from "../system/Config.sol";
 
-contract Factory is UUPSUpgradeable {
+contract Factory is UUPSUpgradeable, ReentrancyGuardUpgradeable {
     AddressBook public addressBook;
 
     event RWADeployed(address indexed token, address indexed owner);
@@ -28,16 +29,18 @@ contract Factory is UUPSUpgradeable {
     }
 
     function initialize(address initialAddressBook) external initializer {
-        __UUPSUpgradeable_init_unchained();
+        __UUPSUpgradeable_init();
+        __ReentrancyGuard_init();
         addressBook = AddressBook(initialAddressBook);
     }
 
-    function deployRWA(bytes calldata signature, uint256 expired) external returns (address proxy) {
+    function deployRWA(bytes calldata signature, uint256 expired) external nonReentrant returns (address) {
         require(block.timestamp <= expired, "Request has expired");
 
-        Config config = addressBook.config();
+        AddressBook _addressBook = addressBook;
+        Config config = _addressBook.config();
 
-        config.holdToken().transferFrom(msg.sender, address(addressBook.treasury()), config.createRWAFee());
+        config.holdToken().transferFrom(msg.sender, address(_addressBook.treasury()), config.createRWAFee());
 
         bytes32 messageHash = MessageHashUtils.toEthSignedMessageHash(
             keccak256(
@@ -46,56 +49,59 @@ contract Factory is UUPSUpgradeable {
         );
 
         require(
-            SignatureChecker.isValidSignatureNow(addressBook.backend(), messageHash, signature), 
+            SignatureChecker.isValidSignatureNow(_addressBook.backend(), messageHash, signature),
             "Backend signature check failed"
         );
 
-        ERC1967Proxy proxy = new ERC1967Proxy(addressBook.rwaImplementation(), "");
-        RWA(address(proxy)).initialize(address(addressBook), msg.sender, ""); // todo rplce token uri
+        address proxy = address(new ERC1967Proxy(_addressBook.rwaImplementation(), ""));
+        RWA rwa = RWA(proxy);
+        rwa.initialize(address(_addressBook), msg.sender, "");
 
-        emit RWADeployed(address(proxy), msg.sender);
+        emit RWADeployed(proxy, msg.sender);
 
-        addressBook.addRWA(RWA(address(proxy)));
-        return address(proxy);
+        _addressBook.addRWA(rwa);
+        return proxy;
     }
 
     function deployPool(
         bytes calldata signature,
         RWA rwa,
         uint256 targetAmount,
-        uint256 profitPercent, 
+        uint256 profitPercent,
         uint256 investmentDuration,
         uint256 realiseDuration,
         uint256 expired,
         bool isStabilityPool
-    ) external returns (address) {
+    ) external nonReentrant returns (address) {
         require(block.timestamp <= expired, "Request has expired");
 
-        Config config = addressBook.config();
-
-        config.holdToken().transferFrom(msg.sender, address(addressBook.treasury()), config.createPoolFee());
+        AddressBook _addressBook = addressBook;
+        Config config = _addressBook.config();
 
         require(
             targetAmount >= config.minTargetAmount() && targetAmount <= config.maxTargetAmount(),
             "Target amount out of allowed range"
         );
+
         require(
-            profitPercent >= config.minProfitPercent() &&
-                profitPercent <= config.maxProfitPercent(),
-            "Profit percentage out of allowed range"    
+            profitPercent >= config.minProfitPercent() && profitPercent <= config.maxProfitPercent(),
+            "Profit percentage out of allowed range"
         );
+
         require(
-            investmentDuration >= config.minInvestmentDuration() &&
-                investmentDuration <= config.maxInvestmentDuration(),
+            investmentDuration >= config.minInvestmentDuration() && investmentDuration <= config.maxInvestmentDuration(),
             "Investment duration out of allowed range"
         );
+
         require(
-            realiseDuration >= config.minRealiseDuration() &&
-                realiseDuration <= config.maxRealiseDuration(),
+            realiseDuration >= config.minRealiseDuration() && realiseDuration <= config.maxRealiseDuration(),
             "Realise duration out of allowed range"
         );
-        require(addressBook.isRWA(address(rwa)), "RWA not registered in system");
+
+        require(_addressBook.isRWA(address(rwa)), "RWA not registered in system");
         require(rwa.productOwner() == msg.sender, "Caller is not RWA owner");
+
+        config.holdToken().transferFrom(msg.sender, address(_addressBook.treasury()), config.createPoolFee());
 
         bytes32 messageHash = MessageHashUtils.toEthSignedMessageHash(
             keccak256(
@@ -106,7 +112,7 @@ contract Factory is UUPSUpgradeable {
                     "deployPool",
                     address(rwa),
                     targetAmount,
-                    profitPercent,
+                    profitPercent, 
                     investmentDuration,
                     realiseDuration,
                     expired,
@@ -116,14 +122,17 @@ contract Factory is UUPSUpgradeable {
         );
 
         require(
-            SignatureChecker.isValidSignatureNow(addressBook.backend(), messageHash, signature),
+            SignatureChecker.isValidSignatureNow(_addressBook.backend(), messageHash, signature),
             "Backend signature check failed"
         );
 
-        address proxy = address(new ERC1967Proxy(addressBook.poolImplementation(), ""));
+        address proxy = address(new ERC1967Proxy(_addressBook.poolImplementation(), ""));
         uint256 rwaSupply = config.rwaInitialSupply();
         uint256 rwaId = rwa.createToken(proxy, rwaSupply);
-        Pool(proxy).initialize(
+
+        Pool pool = Pool(proxy);
+
+        pool.initialize(
             address(addressBook),
             address(config.holdToken()),
             address(rwa),
@@ -134,15 +143,15 @@ contract Factory is UUPSUpgradeable {
             rwaSupply,
             targetAmount,
             profitPercent,
-            block.timestamp + investmentDuration, 
+            block.timestamp + investmentDuration,
             block.timestamp + realiseDuration,
-            true // TODO: replace to speculationEnabled
+            true
         );
 
-        emit PoolDeployed(address(proxy), msg.sender, address(rwa), rwaId);
+        emit PoolDeployed(proxy, msg.sender, address(rwa), rwaId);
 
-        addressBook.addPool(Pool(proxy));
-        return address(proxy);
+        _addressBook.addPool(pool);
+        return proxy;
     }
 
     function _authorizeUpgrade(address newImplementation) internal override {
---
+++ test/unit/Factory.test.ts
@@ -0,0 +1,264 @@
+import { expect } from 'chai'
+import { ethers } from 'hardhat'
+import { deployments } from 'hardhat'
+import { time } from '@nomicfoundation/hardhat-network-helpers'
+import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers'
+import {
+  Factory,
+  Factory__factory,
+  AddressBook,
+  AddressBook__factory,
+  Config,
+  Config__factory,
+  RWA,
+  RWA__factory,
+  Pool,
+  Pool__factory,
+  IERC20,
+  IERC20__factory,
+  Treasury,
+  Treasury__factory,
+} from '../../typechain-types'
+import ERC20Minter from '../utils/ERC20Minter'
+import { BigNumberish, EventLog } from 'ethers'
+import { USDT } from '../../constants/addresses'
+
+describe('Factory', function () {
+  let owner: SignerWithAddress
+  let backend: SignerWithAddress
+  let user: SignerWithAddress
+  let factory: Factory
+  let addressBook: AddressBook
+  let config: Config
+  let holdToken: IERC20
+  let treasury: Treasury
+
+  beforeEach(async function () {
+    ;[owner, backend, user] = await ethers.getSigners()
+
+    await deployments.fixture()
+
+    factory = Factory__factory.connect((await deployments.get('Factory')).address, ethers.provider)
+
+    addressBook = AddressBook__factory.connect(
+      (await deployments.get('AddressBook')).address,
+      ethers.provider,
+    )
+
+    config = Config__factory.connect((await deployments.get('Config')).address, ethers.provider)
+
+    holdToken = IERC20__factory.connect(USDT, ethers.provider)
+
+    treasury = Treasury__factory.connect(
+      (await deployments.get('Treasury')).address,
+      ethers.provider,
+    )
+  })
+
+  describe('deployRWA', function () {
+    it('Should deploy new RWA token', async function () {
+      await ERC20Minter.mint(await holdToken.getAddress(), user.address, 1000000)
+      await holdToken.connect(user).approve(await factory.getAddress(), ethers.parseEther('100'))
+
+      const expired = (await time.latest()) + 3600
+      const message = ethers.solidityPackedKeccak256(
+        ['uint256', 'address', 'address', 'string', 'uint256'],
+        [
+          await ethers.provider.getNetwork().then(n => n.chainId),
+          await factory.getAddress(),
+          user.address,
+          'deployRWA',
+          expired,
+        ],
+      )
+      const signature = await backend.signMessage(ethers.getBytes(message))
+
+      const tx = await factory.connect(user).deployRWA(signature, expired)
+      const receipt = await tx.wait()
+
+      const rwaDeployedEvent = receipt?.logs.find(
+        e => e instanceof EventLog && e.eventName === 'RWADeployed',
+      ) as EventLog
+
+      const rwaAddress = rwaDeployedEvent.args[0]
+      const rwa = RWA__factory.connect(rwaAddress, ethers.provider)
+      expect(await rwa.productOwner()).to.equal(user.address)
+    })
+
+    it('Should revert if signature expired', async function () {
+      const expired = (await time.latest()) - 3600
+      const signature = '0x'
+
+      await expect(factory.connect(user).deployRWA(signature, expired)).to.be.revertedWith(
+        'Request has expired',
+      )
+    })
+
+    it('Should revert if invalid signature', async function () {
+      await ERC20Minter.mint(await holdToken.getAddress(), user.address, 1000000)
+      await holdToken.connect(user).approve(await factory.getAddress(), ethers.parseEther('100'))
+
+      const expired = (await time.latest()) + 3600
+      const signature = '0x'
+
+      await expect(factory.connect(user).deployRWA(signature, expired)).to.be.revertedWith(
+        'Backend signature check failed',
+      )
+    })
+  })
+
+  describe('deployPool', function () {
+    let rwa: RWA
+    let targetAmount: BigNumberish
+    let profitPercent: BigNumberish
+    let investmentDuration: BigNumberish
+    let realiseDuration: BigNumberish
+
+    beforeEach(async function () {
+      targetAmount = await config.minTargetAmount()
+      profitPercent = await config.minProfitPercent()
+      investmentDuration = await config.minInvestmentDuration()
+      realiseDuration = await config.minRealiseDuration()
+
+      // Deploy RWA first
+      await ERC20Minter.mint(await holdToken.getAddress(), user.address, 1000000)
+      await holdToken.connect(user).approve(await factory.getAddress(), ethers.parseEther('300')) // For both RWA and Pool fees
+
+      const expired = (await time.latest()) + 3600
+      const message = ethers.solidityPackedKeccak256(
+        ['uint256', 'address', 'address', 'string', 'uint256'],
+        [
+          await ethers.provider.getNetwork().then(n => n.chainId),
+          await factory.getAddress(),
+          user.address,
+          'deployRWA',
+          expired,
+        ],
+      )
+      const signature = await backend.signMessage(ethers.getBytes(message))
+
+      const tx = await factory.connect(user).deployRWA(signature, expired)
+      const receipt = await tx.wait()
+
+      const rwaDeployedEvent = receipt?.logs.find(
+        e => e instanceof EventLog && e.eventName === 'RWADeployed',
+      ) as EventLog
+      const rwaAddress = rwaDeployedEvent.args[0]
+      rwa = RWA__factory.connect(rwaAddress, ethers.provider)
+    })
+
+    it('Should deploy new pool', async function () {
+      const expired = (await time.latest()) + 3600
+      const message = ethers.solidityPackedKeccak256(
+        [
+          'uint256',
+          'address',
+          'address',
+          'string',
+          'address',
+          'uint256',
+          'uint256',
+          'uint256',
+          'uint256',
+          'uint256',
+          'bool',
+        ],
+        [
+          await ethers.provider.getNetwork().then(n => n.chainId),
+          await factory.getAddress(),
+          user.address,
+          'deployPool',
+          await rwa.getAddress(),
+          targetAmount,
+          profitPercent,
+          investmentDuration,
+          realiseDuration,
+          expired,
+          false,
+        ],
+      )
+      const signature = await backend.signMessage(ethers.getBytes(message))
+
+      const tx = await factory
+        .connect(user)
+        .deployPool(
+          signature,
+          rwa,
+          targetAmount,
+          profitPercent,
+          investmentDuration,
+          realiseDuration,
+          expired,
+          false,
+        )
+
+      const receipt = await tx.wait()
+
+      const event = receipt?.logs.find(
+        e => e instanceof EventLog && e.eventName === 'PoolDeployed',
+      ) as EventLog
+
+      const poolAddress = event.args[0]
+      const pool = Pool__factory.connect(poolAddress, ethers.provider)
+
+      expect(await pool.rwa()).to.equal(await rwa.getAddress())
+      expect(await pool.targetAmount()).to.equal(targetAmount)
+      expect(await pool.profitPercent()).to.equal(profitPercent)
+    })
+
+    it('Should revert if target amount out of range', async function () {
+      const expired = (await time.latest()) + 3600
+      const signature = '0x'
+
+      await expect(
+        factory.connect(user).deployPool(
+          signature,
+          rwa,
+          1, // Too small
+          profitPercent,
+          investmentDuration,
+          realiseDuration,
+          expired,
+          false,
+        ),
+      ).to.be.revertedWith('Target amount out of allowed range')
+    })
+
+    it('Should revert if profit percent out of range', async function () {
+      const expired = (await time.latest()) + 3600
+      const signature = '0x'
+
+      await expect(
+        factory.connect(user).deployPool(
+          signature,
+          rwa,
+          targetAmount,
+          10000000, // Too high
+          investmentDuration,
+          realiseDuration,
+          expired,
+          false,
+        ),
+      ).to.be.revertedWith('Profit percentage out of allowed range')
+    })
+
+    it('Should revert if not RWA owner', async function () {
+      const expired = (await time.latest()) + 3600
+      const signature = await backend.signMessage(ethers.getBytes('0x'))
+
+      await expect(
+        factory.connect(owner).deployPool(
+          // Using different signer
+          signature,
+          rwa,
+          targetAmount,
+          profitPercent,
+          investmentDuration,
+          realiseDuration,
+          expired,
+          false,
+        ),
+      ).to.be.revertedWith('Caller is not RWA owner')
+    })
+  })
+})
